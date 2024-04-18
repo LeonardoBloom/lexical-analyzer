9d41e5084479977ccf2c0b57ce9e6cf0acd59171
LeonardoBloom <leonardocolacogg@gmail.com>
2024-04-18 11:34:41 +0300

    Add files via upload

--- lexical_analyzer.py
+++ lexical_analyzer.py
@@ -1,246 +1,106 @@
-import json # imported for file writing and reading
-from datetime import datetime # imported for time calculations
 
-class Student:
+from enum import Enum # imported Enum for use of Enumerations
 
-    # constructor that initializes all the properties of a student
-    def __init__(self, student_number, first_name, last_name, date_of_birth, sex, country_of_birth):
-        self.student_number = student_number
-        self.first_name = first_name
-        self.last_name = last_name
-        self.date_of_birth = date_of_birth
-        self.sex = sex
-        self.country_of_birth = country_of_birth
+# TOKENS defined for enumeration of types
+class TOKENS(Enum):
+    INTEGER = 1
+    FLOAT = 2
+    ID = 3
+    BITWISE_OR = 4
+    LOGICAL_OR = 5
+    BITWISE_AND = 6
+    LOGICAL_AND = 7
+    FOR = 8
+    WHILE = 9
+    IF = 10
+    ELSE = 11
+    ERROR = 12
 
-    # getter methods for each Student property
-    def get_student_number(self):
-        return self.student_number
-    def get_first_name(self):
-        return self.first_name
-    def get_last_name(self):
-        return self.last_name
-    def get_date_of_birth(self):
-        return self.date_of_birth
-    def get_sex(self):
-        return self.sex
-    def get_country_of_birth(self):
-        return self.country_of_birth
-
-    # setter methods for each Student property
-    def set_student_number(self, student_number):
-        self.student_number = student_number
-    def set_first_name(self, first_name):
-        self.first_name = first_name
-    def set_last_name(self, last_name):
-        self.last_name = last_name
-    def set_date_of_birth(self, date_of_birth):
-        self.date_of_birth = date_of_birth
-    def set_sex(self, sex):
-        self.sex = sex
-    def set_country_of_birth(self, country_of_birth):
-        self.country_of_birth = country_of_birth
-
-# initializing an array that stores each of the Student objects
-students_array = []
-
-# function to write student data to a file
-def write_to_file(file_path, data):
-    with open(file_path, 'w') as file:
-        json.dump(data, file, default=lambda obj: obj.__dict__)
-        # python lambda is used to serialize our Student object with key-pair values suitable for json files
-
-# function to read student data from a file and populate the array
-def read_from_file(file_path):
-    try:
-        # open and read from json file
-        with open(file_path, 'r') as file:
-            data = json.load(file)
-            print("Loaded data from file:", data)  # debugging to see if the data loaded is what we want
-            students = []
-            if isinstance(data, list):  # debugging to check if the loaded data is a list
-                for student_data in data:
-                    if isinstance(student_data, dict):  # check if each item is a dictionary
-                        # append the student array with objects possessign the Student parameters (**)
-                        students.append(Student(**student_data)) 
-                    else:
-                        print("Invalid data format in the file.")
-                        return []
-            else:
-                print("Invalid data format in the file.")
-                return []
-            
-            # return the array containing student data
-            return students
-    # try catch error
-    except FileNotFoundError:
-        return []
+class LEX_RESULT:
+    # constructor for LEX_RESULT, showing all the properties of the desired return object
+    def __init__(self, token, symbol_index=None, integer_value=None, float_value=None, string_char=None):
+        self.token = token
+        self.symbol_index = symbol_index
+        self.integer_value = integer_value
+        self.float_value = float_value
+        self.string_char = string_char # the unrecognized string
 
 
+# lex is the method that identifies each token and lexeme and then
+# adds the information about them in the LEX_RESULT object
+def lex(file_content, symbol_table):
+    # dictionary created assigning the tokens to the token types
+    tokens = {
+        '|': TOKENS.BITWISE_OR,
+        '||': TOKENS.LOGICAL_OR,
+        '&': TOKENS.BITWISE_AND,
+        '&&': TOKENS.LOGICAL_AND,
+        'for': TOKENS.FOR,
+        'while': TOKENS.WHILE,
+        'if': TOKENS.IF,
+        'else': TOKENS.ELSE
+    }
 
+    # go through each lexeme in the file
+    for lexeme in file_content.split():
+        
+        # check if lexeme is integer
+        if lexeme.isdigit() or (lexeme[0] == '-' and lexeme[1:].isdigit()):
+            yield LEX_RESULT(TOKENS.INTEGER, integer_value=int(lexeme))
 
-# function to calculate age based on the date of birth
-def calculate_age(date_of_birth):
-    today = datetime.now() # date of TODAY
-    # parses the input date of birth and converts it into a time python can read using datetime
-    birth_date = datetime.strptime(date_of_birth, "%Y-%m-%d")
-    
-    # calculating the student age
-    age = today.year - birth_date.year
+        # check for float
+        elif lexeme.replace('.', '', 1).isdigit():
+            yield LEX_RESULT(TOKENS.FLOAT, float_value=float(lexeme))
 
-    return age
+        # check if lexeme is a token
+        elif lexeme in tokens:
+            yield LEX_RESULT(tokens[lexeme])
 
-# function to add a new student
-def add_student():
+        
+        elif lexeme.isidentifier():  # Check if lexeme is a valid identifier
+            if lexeme not in symbol_table:
+                symbol_table.append(lexeme)  # Add identifier to symbol table
+            yield LEX_RESULT(TOKENS.ID, symbol_index=symbol_table.index(lexeme))
+        else:
+            # if lexeme is not an expected token or identifier
+            yield LEX_RESULT(TOKENS.ERROR, string_char=lexeme)
 
-    # first, check if student array is full
-    if len(students_array) < 101:
-        # user inputs
-        student_number = int(input("Enter student number: "))
-        first_name = input("Enter first name: ")
-        last_name = input("Enter last name: ")
-        date_of_birth = input("Enter date of birth (YYYY-MM-DD): ")
-        sex = input("Enter sex: ")
-        country_of_birth = input("Enter country of birth: ")
+# initialize symbol table with these reserved words
+symbol_table = ["for", "while", "if", "else"]
 
-        new_student = Student(student_number, first_name, last_name, date_of_birth, sex, country_of_birth)
-        # after creating a student object with the user input, add to the array holding all students
-        students_array.append(new_student)
-        print("New student added successfully.")
-    else:
-        print("Sorry, we cant add any more students")
+# read the contents of our input text file
+with open("assignment2.txt", "r") as file:
+    file_content = file.read()
 
-# function to find a student by student number
-def find_student_by_number():
-    student_number = int(input("Enter student number to find: "))
-    
-    for student in students_array:
-        # if an element (each "student") in the student array possesses the stu number searched, proceed
-        if student.get_student_number() == student_number:
-            print(f"Student found!\n")
-            print(f"Student Number: {student.get_student_number()}")
-            print(f"Name: {student.get_first_name()} {student.get_last_name()}")
-            print(f"Date of Birth: {student.get_date_of_birth()}")
-            print(f"Sex: {student.get_sex()}")
-            print(f"Country of Birth: {student.get_country_of_birth()}")
-            print(f"Age: {calculate_age(student.get_date_of_birth())} years")
-            return
-    # otherwise, print no student found
-    print(f"No student found with student number {student_number}.")
-
-# function that shows all students
-def show_all_students():
-    # if there is no student in the array, print no students
-    if not students_array:
-        print("No students in the array.")
-        return
-
-    # display every student element in the students_array
-    for student in students_array:
-        print(f"Student Number: {student.get_student_number()}")
-        print(f"Name: {student.get_first_name()} {student.get_last_name()}")
-        print(f"Date of Birth: {student.get_date_of_birth()}")
-        print(f"Sex: {student.get_sex()}")
-        print(f"Country of Birth: {student.get_country_of_birth()}")
-        # calculate_age is a function that calculates the age of a student based on their DOB ^^
-        print(f"Age: {calculate_age(student.get_date_of_birth())} years")
-        print("\n")
-
-# function that shows students born in a given year
-def show_students_by_birth_year():
-    birth_year = input("Enter the birth year to filter students: ")
-
-    # filter array that obtains every student with a specific birth year, hense we use .startswith()
-    filtered_students = [student for student in students_array if student.get_date_of_birth().startswith(birth_year)]
-    
-    # if there are no students with the birth year, print this:
-    if not filtered_students:
-        print(f"No students found born in the year {birth_year}.")
-        return
-    
-    # display the filtered students data
-    for student in filtered_students:
-        print(f"Student Number: {student.get_student_number()}")
-        print(f"Name: {student.get_first_name()} {student.get_last_name()}")
-        print(f"Date of Birth: {student.get_date_of_birth()}")
-        print(f"Sex: {student.get_sex()}")
-        print(f"Country of Birth: {student.get_country_of_birth()}")
-        # calculate_age is a function that calculates the age of a student based on their DOB ^^
-        print(f"Age: {calculate_age(student.get_date_of_birth())} years")
-        print("\n")
-
-# function to modify a student record
-def modify_student_record():
-    student_number = int(input("Enter student number to modify: "))
-
-    # looks for student inside the array holding students
-    for student in students_array:
-        if student.get_student_number() == student_number:
-            field_to_modify = input("Enter the field to modify (first_name, last_name, date_of_birth, sex, country_of_birth): ")
-
-            new_value = input(f"Enter the new value for {field_to_modify}: ")
-
-            # Use setter methods to modify the record
-            if field_to_modify == 'first_name':
-                student.set_first_name(new_value)
-            elif field_to_modify == 'last_name':
-                student.set_last_name(new_value)
-            elif field_to_modify == 'date_of_birth':
-                student.set_date_of_birth(new_value)
-            elif field_to_modify == 'sex':
-                student.set_sex(new_value)
-            elif field_to_modify == 'country_of_birth':
-                student.set_country_of_birth(new_value)
-            print("Student record modified successfully.")
-            return
-    print(f"No student found with student number {student_number}.")
-
-# function for deleting a student with a specific student number
-def delete_student():
-    student_number = int(input("Enter student number to delete: "))
-
-    for student in students_array:
-        if student.get_student_number() == student_number:
-            students_array.remove(student) # removes the student from the array
-            print("Student deleted successfully.")
-            return
-    print(f"No student found with student number {student_number}.")
-
-# the menu when the program starts
+# program loop starts here
 while True:
-    print("\nMenu:")
-    print("1. Write the contents of the student array to a file")
-    print("2. Read student data from a file and populate the student array")
-    print("3. Add a new student")
-    print("4. Find a student by student number")
-    print("5. Show all students")
-    print("6. Show all students born in a certain year")
-    print("7. Modify a student record")
-    print("8. Delete a student record")
-    print("9. Quit")
 
-    choice = input("Enter your choice (1-9): ")
+    # interface for the menu
+    print("1. Call lex()")
+    print("2. Show Symbol Table")
+    print("3. Exit")
 
-    # based on choice, it will run the following:
-    if choice == '1':
-        write_to_file('./student_data.json', students_array)
-        print("Student data written to file.")
-    elif choice == '2':
-        students_array = read_from_file('./student_data.json')
-        print("Student data read from file and populated in the array.")
-    elif choice == '3':
-        add_student()
-    elif choice == '4':
-        find_student_by_number()
-    elif choice == '5':
-        show_all_students()
-    elif choice == '6':
-        show_students_by_birth_year()
-    elif choice == '7':
-        modify_student_record()
-    elif choice == '8':
-        delete_student()
-    elif choice == '9':
-        print("Exiting the program, thanks ! Goodbye!")
-        break
-    else:
-        print("Invalid choice. Please enter a number between 1 and 9.")
+    choice = input("Enter your choice (1-3)")
+
+
+    match choice:
+        case '1':
+
+            # if choice is 1, Call the lex() function, taking the input file content 
+            # and the symbol table as parameters
+
+            for result in lex(file_content, symbol_table):
+                
+                # print the object properties for the lexeme every time lex() is called
+                print(f"<token={result.token.name}, index={result.symbol_index}, "
+                    f"integer_value={result.integer_value}, float_value={result.float_value}, "
+                    f"unrecognized_string='{result.string_char}'>")
+        case '2':
+            # show our symbol table. It will be updated if lex() is called
+            print("Symbol Table:")
+            print(symbol_table)
+            
+        case '3':
+            # Exit the program
+            print("Exiting Program...")
+            break
